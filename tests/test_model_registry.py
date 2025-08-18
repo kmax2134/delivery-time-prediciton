e08de7d927ddd099ef547268c470f012eeee071c
manishity11 <160490232+manishity11@users.noreply.github.com>
2025-08-18 10:51:55 +0530

    change minor code

--- tests/test_model_registry.py
+++ tests/test_model_registry.py
@@ -15,7 +15,7 @@ def load_model_information(file_path):
         
     return run_info
 
-# set model name
+# set model
 model_name = load_model_information("run_information.json")["model_name"]
 
 
